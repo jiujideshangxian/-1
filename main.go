112260d3cc189f2dd47a4cd302fab05f70eed490
jiujideshangxian <1529124973@qq.com>
2020-11-07 22:31:27 +0800

    随时1

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr:=[]int64{10,5,9,6,7}
+	fmt.Println("初始值为",arr)
+	 res:=0
+	 for i:=0;i<len(arr);i++{
+		 for j:=0;j<len(arr)-1;j++ {
+			 if arr[j]>arr[j+1]{
+				 arr[j],arr[j+1]=arr[j+1],arr[j]
+			 }
+		 }
+		 res++
+		 fmt.Println("冒泡排序的第",res,"次结果为",arr)
+	 }
+
+}
