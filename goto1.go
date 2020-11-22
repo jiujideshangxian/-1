bc2dcd87b61c9471adf5125c13a0b14cf8359f69
jiujideshangxian <1529124973@qq.com>
2020-11-22 20:28:48 +0800

    1

--- goto1.go
+++ goto1.go
@@ -5,14 +5,13 @@ import "fmt"
 func main() {
 	//输出1~50的素数
 	var A,a int
-	A=1
-
+	A = 1 //不写人for循环是因为会将A变为1
 	LOOP:
 		for A < 50 {
                A++
 			for a = 2;a<A;a++ {
 				if A%a==0 {
-					goto LOOP
+					goto LOOP//发现因子则不是素数
 				}
 			}
 			fmt.Printf(" %d \t",  A)
